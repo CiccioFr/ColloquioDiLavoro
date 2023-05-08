4e7ed75bfbe5e5dc46fcd87add6c10efeb5ef007
CiccioFr <cicciofr.net@tiscali.it>
2023-05-08 22:24:58 +0200

    GestioneFile - JavaDoc

--- src/main/java/cicciofr/colloquioDiLavoro/model/gestioneFile/GestioneFile.java
+++ src/main/java/cicciofr/colloquioDiLavoro/model/gestioneFile/GestioneFile.java
@@ -1,9 +1,10 @@
 package main.java.cicciofr.colloquioDiLavoro.model.gestioneFile;
 
+import main.java.cicciofr.colloquioDiLavoro.entity.Candidato;
+
 import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class GestioneFile implements IGestioneFile {
@@ -20,6 +21,12 @@ public class GestioneFile implements IGestioneFile {
         return instance;
     }
 
+    /**
+     * Cerca il file e ne inserisce il contenuto in una List, ponendo ogni riga in un bucket
+     *
+     * @param pathFile Path/nomeFile da caricare
+     * @return List delle righe del file
+     */
     @Override
     public List<String> caricaFile(String pathFile) {
 
@@ -66,15 +73,35 @@ public class GestioneFile implements IGestioneFile {
 ////            System.err.format("IOException: %s%n", e);
 //            e.printStackTrace();
 //        }
-
         return contenutoFile;
     }
 
     @Override
     public void salvaFile(String file) {
+        Candidato candidato = new Candidato();
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("nomeFile.txt");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(candidato);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void pulisciString(String file) {
 
     }
 }
+
+
+/*
+        String patternToSerach = "lettere";
+        Pattern pattern = Pattern.compile(patternToSerach); // qui va una ReGeX
+        Matcher matcher = pattern.matcher("Stringa in cui cercare ..");
+        boolean trovato = matcher.find();
+ */
